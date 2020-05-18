ac9a4ee0046e1933849f138bcae83293dbef01aa
Dione Wilson <dionew1@gmail.com>
2020-05-18 08:53:13 -0600

    Complete building an api exercise with items

---
+++ spec/factories/items.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :item do
+    name { "Banana Stand" }
+    description { "There's always money in the banana stand." }
+  end
+end
---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+    expect(items.count).to eq(3)
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
+  end
+
+  it "can create a new item" do
+    item_params = { name: "Saw", description: "I want to play a game" }
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    expect(response).to be_successful
+    expect(item.name).to eq(item_params[:name])
+  end
+
+  it "can update an existing item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Sledge" }
+
+    put "/api/v1/items/#{id}", params: {item: item_params}
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Sledge")
+  end
+
+  it "can destroy an item" do
+    item = create(:item)
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end
